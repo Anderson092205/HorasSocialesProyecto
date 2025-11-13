cf93b7cd78d73eb7847a621853a10644822951af
Anderson092205 <andersonalfredoescobarmoran@gmail.com>
2025-11-12 19:58:22 -0600

    subiendo la conexion

---
+++ src/main/java/sv/gob/cementerios/cementeriosle/controller/TestConnectionController.java
@@ -0,0 +1,23 @@
+package sv.gob.cementerios.cementeriosle.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@RestController
+public class TestConnectionController {
+
+    @GetMapping("/test")
+    public String testSqlConnection() {
+        String url = "jdbc:sqlserver://ANDERSON:1433;databaseName=CementerioLibertadEste;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            return "✅ Conexión exitosa con SQL Server";
+        } catch (SQLException e) {
+            return "❌ Error de conexión: " + e.getMessage();
+        }
+    }
+}
