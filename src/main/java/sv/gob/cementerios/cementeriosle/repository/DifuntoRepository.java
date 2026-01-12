b7e195d75ab711713a51a6472d92af5d255548f6
XxxDavoxxX <davidfranco2802@gmail.com>
2026-01-11 22:20:11 -0600

    Update DifuntoRepository.java
    Agregando class java Repository parte 2

--- src/main/java/sv/gob/cementerios/cementeriosle/repository/DifuntoRepository.java
+++ src/main/java/sv/gob/cementerios/cementeriosle/repository/DifuntoRepository.java
@@ -2,12 +2,23 @@ package sv.gob.cementerios.cementeriosle.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import sv.gob.cementerios.cementeriosle.model.Difunto;
+
 import java.util.List;
 
 @Repository
-public interface DifuntoRepository extends JpaRepository<Difunto, Integer> {
+public interface DifuntoRepository extends JpaRepository<Difunto, Long> {
 
-    /** Busca todos los difuntos por el ID del cementerio. */
-    List<Difunto> findByIdCementerio(Integer idCementerio);
+    // ✅ Cuenta difuntos en un espacio específico
+    long countByEspacio_IdEspacio(Long idEspacio);
+
+    // ✅ Obtener difuntos por cementerio
+    @Query("SELECT d FROM Difunto d WHERE d.espacio.celda.parcela.cementerio.idCementerio = :idCementerio")
+    List<Difunto> findByIdCementerio(@Param("idCementerio") Integer idCementerio);
 }
+
+
+
+
